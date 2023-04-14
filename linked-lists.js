62841e028aed313790c1b3cf2f72549853666cfc
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-04-14 14:26:19 -0300

    Create base factory functions

---
+++ linked-lists.js
@@ -0,0 +1,10 @@
+// We first need to create two factory functions: one for the full list
+// and other for each node.
+
+function linkedList() {
+
+};
+
+function Node(value = null, nextNode = null) {
+
+};
