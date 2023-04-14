553f787743c26f772e2804f80584e50d6d69b664
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-04-14 14:45:14 -0300

    Add append node function

--- linked-lists.js
+++ linked-lists.js
@@ -1,12 +1,43 @@
 // We first need to create two factory functions: one for the full list
 // and other for each node.
 
-function linkedList() {
-    this.head = null;
-    this.size = 0;
-};
+// Let's start defining our nodes: we will create them with a value and 
+// an empty pointer to the next node.
 
 function Node(value, next = null) {
     this.value = value;
     this.next = next;
+};
+
+// Now we start with out linked list: we will create an empty list first,
+// with a null head variable and a size of 0.
+
+// When we append a value we will create the node and look for the head pointer.
+// If there is no head we will asign the new node as it, and if there is one we
+// will store it in a temp variable. We will iterate through the list until there
+// is no more 'next' properties and then will append the new node.
+
+function linkedList() {
+    this.head = null;
+    this.size = 0;
+    
+    // Functions:
+
+    const append = function (value) {    
+        let temp = 0;
+        let node = new Node(element);
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            temp = this.head;
+        };
+
+        while (temp.next) {
+            temp = temp.next;
+        }
+
+        temp.next = node;
+    };
+
 };
