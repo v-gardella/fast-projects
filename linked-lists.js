666ef3875caa12787b365522d5339013a44d62ea
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-04-14 15:01:03 -0300

    Add node counter function

--- linked-lists.js
+++ linked-lists.js
@@ -17,6 +17,10 @@ function Node(value, next = null) {
 // will store it in a temp variable. We will iterate through the list until there
 // is no more 'next' properties and then will append the new node.
 
+// If we want to append a value at the beginning of the list, first we need to 
+// create the node, then we define that the next node of the new one will be the head
+// node we had before, and reasign the head value to the new node.
+
 function linkedList() {
     this.head = null;
     this.size = 0;
@@ -25,7 +29,7 @@ function linkedList() {
 
     const append = function (value) {    
         let temp = 0;
-        let node = new Node(element);
+        let node = new Node(value);
 
         if (this.head === null) {
             this.head = node;
@@ -40,4 +44,27 @@ function linkedList() {
         temp.next = node;
     };
 
+    const prepend = function (value) {
+        let node = new Node(value);
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+    };
+
+    const size = function () {
+        let temp = this.head;
+        let counter = 1;
+
+        if (this.head === null) return 0;
+        while (temp.next) {
+            counter += 1;
+            temp = temp.next;
+        }
+
+        return counter;
+    }
 };
