e09327a927321aadf16d8eebe4eb34089a2df334
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-04-18 10:17:17 -0300

    Add 'buildTree' function and merge sort algorithm

--- binary-tree.js
+++ binary-tree.js
@@ -9,3 +9,41 @@ function Node(value) {
     this.right = null;
     this.left = null;
 }
+
+// Then we create the tree factory function and the tree construction
+// function.
+
+function buildTree(array) {
+    
+}
+
+// Let's add a Merge Sort script to order the array.
+
+function mergeSort(arr) {
+    if (arr.length < 2) return arr;
+    if (arr.length > 1) {
+        const mid = Math.floor(arr.length/2);
+        let leftArr = arr.slice(0, mid);
+        let rightArr = arr.slice(mid, arr.length)
+        return merge(mergeSort(leftArr), mergeSort(rightArr))
+    }
+}
+
+function merge(left, right) {
+    let sortedArr = [];
+
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        sortedArr.push(left.shift())
+      } else {
+        sortedArr.push(right.shift())
+      }
+    }
+
+    return [...sortedArr, ...left, ...right]
+}
+
+
+// Function Calls
+
+console.log(mergeSort([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]))
