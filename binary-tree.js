100b1ee39f983dbbddf2e680069e1bfb21ede083
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-04-18 14:35:59 -0300

    Delete tree visualization function

--- binary-tree.js
+++ binary-tree.js
@@ -73,22 +73,9 @@ function merge(left, right) {
 }
 
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node === null) {
-       return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-  }
-
-
 // Function Calls
 
 let prueba = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 50];
 let newTree = new Tree(prueba);
 console.log(newTree)
+console.log(newTree.left);
