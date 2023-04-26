5c1f16d42d2bdf224b7a02b5d3bf4dad7828b1d6
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-04-26 12:35:14 -0300

    fix: change test message

--- jest/reverse-string.test.js
+++ jest/reverse-string.test.js
@@ -12,7 +12,7 @@ test('Function respects special characters', () => {
     expect(reverseString('Hello! World?')).toBe('?dlroW !olleH')
 })
 
-test('Reversed string respects uppercase letters', () => {
+test('Reversed string respects upper, lower and propercase', () => {
     expect(reverseString('this one is in lowecase')).toBe('esacewol ni si eno siht');
     expect(reverseString('This One Is In Propercase')).toBe('esacreporP nI sI enO sihT');
     expect(reverseString('THIS ONE IS IN UPPERCASE')).toBe('ESACREPPU NI SI ENO SIHT');
