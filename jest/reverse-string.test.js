8ead5c63b5c8e9a95d53565d513ef538f830add5
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-04-26 12:19:11 -0300

    feat: add tests

---
+++ jest/reverse-string.test.js
@@ -0,0 +1,19 @@
+const reverseString = require('./reverse-string');
+
+test('The function returns a reversed string', () => {
+    expect(reverseString('watermelon')).toBe('nolemretaw')
+});
+
+test('Reversed string respects spaces', () => {
+    expect(reverseString('Look, a string')).toBe('gnirts a ,kooL')
+})
+
+test('Function respects special characters', () => {
+    expect(reverseString('Hello! World?')).toBe('?dlroW !olleH')
+})
+
+test('Reversed string respects uppercase letters', () => {
+    expect(reverseString('this one is in lowecase')).toBe('esacewol ni si eno siht');
+    expect(reverseString('This One Is In Propercase')).toBe('esacreporP nI sI enO sihT');
+    expect(reverseString('THIS ONE IS IN UPPERCASE')).toBe('ESACREPPU NI SI ENO SIHT');
+})
